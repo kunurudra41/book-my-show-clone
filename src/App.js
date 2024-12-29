855e9aa8b810239ad068c4b927d12c5483cf87c3
Rudra Narayan Jena <kunurudra41@gmail.com>
2024-12-29 17:57:59 +0000

    Day-01

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Routes,Route} from 'react-router-dom';
+
+//react-slick css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
+ 
+  );
+}
+
+export default App;
